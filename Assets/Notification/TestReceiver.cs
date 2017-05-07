c6fe9aeb1b2fc3a55bb5153ea251538b5df0577d
BlackZIjian <735809461@qq.com>
2017-05-07 11:14:40 +0800

    添加消息机制

---
+++ Assets/Notification/TestReceiver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestReceiver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NotificationCenter.DefaultCenter().AddObserver(this, "testMessage");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void testMessage(Notification noti)
+    {
+        Debug.Log(noti.sender);
+    }
+}
