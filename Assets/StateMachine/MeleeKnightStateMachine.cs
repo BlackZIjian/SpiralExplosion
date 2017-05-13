1a3550fe1fe2050799a94302130b4ddec2b98f14
BlackZIjian <735809461@qq.com>
2017-05-13 15:36:38 +0800

    完成怪物基本AI状态机

--- Assets/StateMachine/MeleeKnightStateMachine.cs
+++ Assets/StateMachine/MeleeKnightStateMachine.cs
@@ -1,8 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Collections.Generic;
 
 
+public class ViewCast
+{
+    public static List<Transform> GetConeCast(float maxDis,float angle,Vector3 direction,Vector3 center,LayerMask layer)
+    {
+        List<Transform> results = new List<Transform>();
+        Collider[] colliders = Physics.OverlapSphere(center, maxDis, layer);
+        float cos = Mathf.Cos(angle / 2 / 180 * Mathf.PI);
+       
+        foreach (Collider c in colliders)
+        {
+            Vector3 target = c.gameObject.transform.position;
+            if (Vector3.Dot(direction,target) / direction.magnitude / target.magnitude >= cos )
+            {
+                results.Add(c.gameObject.transform);
+            }
+        }
+        return results;
+    }
+    public static bool isConeCast(float maxDis, float angle, Vector3 direction, Vector3 center, LayerMask layer)
+    {
+        Collider[] colliders = Physics.OverlapSphere(center, maxDis, layer);
+        float cos = Mathf.Cos(angle / 2 / 180 * Mathf.PI);
+
+        foreach (Collider c in colliders)
+        {
+            Vector3 target = c.gameObject.transform.position;
+            if (Vector3.Dot(direction, target) / direction.magnitude / target.magnitude >= cos)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 public struct AIInput
 {
     public Vector2 AIInputV;
@@ -13,7 +48,12 @@ public class MeleeAISubStateMachine : FSMSystem
 {
     public MeleeMoveSubStateMachine moveFSM;
     public SuperCharacterController MeleeController;
+    public LayerMask canSee;
     public AIInput ai_input = new AIInput();
+    public bool enableSuperMove = true;
+    public float maxSeeDis = 10;
+    public float angle = 30;
+    public float jiejinDis = 3;
     public void Start()
     {
         moveFSM = gameObject.AddComponent<MeleeMoveSubStateMachine>();
@@ -25,6 +65,11 @@ public class MeleeAISubStateMachine : FSMSystem
     {
         CurrentState.Reason();
         CurrentState.Act();
+        if(enableSuperMove)
+        {
+            moveFSM.CurrentState.Reason();
+            moveFSM.CurrentState.Act();
+        }
     }
 }
 
@@ -38,25 +83,27 @@ public class MeleeMoveSubStateMachine : FSMSystem
 public class MeleeKnightIdleState : FSMState
 {
     public SuperCharacterController controller;
-    float ronateSpeed;
-    SightCone sight;
+    MeleeAISubStateMachine aiFSM;
     float remainTime = 0;
-    
+
     public MeleeKnightIdleState(SuperCharacterController c, FSMSystem f)
     {
         fsm = f;
+        aiFSM = (MeleeAISubStateMachine)fsm;
         controller = c;
         stateID = StateID.MeleeKnightIdle;
         AddTransition(Transition.MeleeKnightSeeSomeOne, StateID.MeleeKnightSeeSomeOne);
     }
     public override void Reason()
     {
-
+        if(ViewCast.isConeCast(aiFSM.maxSeeDis, aiFSM.angle, controller.lookDirection,controller.transform.position,aiFSM.canSee))
+        {
+            aiFSM.PerformTransition(Transition.MeleeKnightSeeSomeOne);
+        }
     }
 
     public override void Act()
     {
-        MeleeAISubStateMachine aiFSM = (MeleeAISubStateMachine)fsm;
         if(remainTime <= 0)
         {
             aiFSM.ai_input.AIInputV = new Vector2(0, 0);
@@ -75,7 +122,57 @@ public class MeleeKnightIdleState : FSMState
     }
     public override void DoBeforeEntering()
     {
-        sight = controller.gameObject.GetComponent<SightCone>();
+        controller.enabled = true;
+        aiFSM.enableSuperMove = true;
+    }
+}
+
+public class MeleeKnightSeeSomeoneState : FSMState
+{
+    public SuperCharacterController controller;
+    MeleeAISubStateMachine aiFSM;
+    Vector3 lastPos;
+
+    public MeleeKnightSeeSomeoneState(SuperCharacterController c, FSMSystem f)
+    {
+        fsm = f;
+        aiFSM = (MeleeAISubStateMachine)fsm;
+        controller = c;
+        stateID = StateID.MeleeKnightSeeSomeOne;
+    }
+    public override void Reason()
+    {
+        List<Transform> canSees = ViewCast.GetConeCast(aiFSM.maxSeeDis, aiFSM.angle, controller.lookDirection, controller.transform.position, aiFSM.canSee);
+        int maxLevel = -1;
+        foreach(Transform trans in canSees)
+        {
+            float dis = Vector3.Distance(controller.transform.position, trans.position);
+            int level = 2;//看不清
+            if ( dis < aiFSM.maxSeeDis )
+            {
+                level = -1;//get看见物体的信息
+            }
+            if(level > maxLevel)
+            {
+                lastPos = trans.position;
+            }
+        }
+        if(Vector3.Distance(controller.transform.position, lastPos) < aiFSM.jiejinDis)
+        {
+            //根据看见物体信息转移状态
+        }
+    }
+
+    public override void Act()
+    {
+        //开启寻路,寻向lastPos
+
+    }
+    public override void DoBeforeEntering()
+    {
+        controller.enabled = true;
+        aiFSM.enableSuperMove = true;
+        lastPos = controller.transform.position;
     }
 }
 
